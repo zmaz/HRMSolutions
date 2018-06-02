3a22f1b8c3ae9fa390fc1ad6ddd7e071e8a0f2c1
zmaz <praveen.lama91@gmail.com>
2018-06-02 21:41:22 +0545

    Create 12-TetraContiplier.asm

---
+++ 12-TetraContiplier.asm
@@ -0,0 +1,15 @@
+a:
+  INBOX
+  COPYTO    0
+  ADD       0
+  ADD       0
+  ADD       0
+  ADD       0
+  COPYTO    1
+  ADD       1
+  COPYTO    2
+  ADD       2
+  COPYTO    3
+  ADD       3
+  OUTBOX
+  JUMP      a
